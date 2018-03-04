6cbf83dcbc57f05e86733694145462df47c282f5
vanithasajja <vanitha_1010@yahoo.com>
2018-03-04 03:59:44 -0500

    MVCMusicStoreApplication (Lab 4)

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCMusicStoreApplication.MvcApplication" Language="C#" %>
---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStoreApplication.Models
+{
+    public class Genre
+    {
+        public virtual int GenreId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }        
+    }
+}
