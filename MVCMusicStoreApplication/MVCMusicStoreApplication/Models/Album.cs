39b326546784cf9e8eaf44187b1414dd503fd210
vanithasajja <vanithasajja2010@gmail.com>
2018-03-04 22:47:33 -0500

    MVCMusicStoreApplication (Lab 7)

--- MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Album.cs
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Album.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace MVCMusicStoreApplication.Models
 {
+    [Bind(Exclude = "AlbumId")]
     public class Album
     {
         public virtual int AlbumId { get; set; }
