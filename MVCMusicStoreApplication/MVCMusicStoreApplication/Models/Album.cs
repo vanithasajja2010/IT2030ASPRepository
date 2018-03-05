bd6f0ef0c279d2eb75eae14583c03872cab2a530
vanithasajja <vanithasajja2010@gmail.com>
2018-03-04 20:11:16 -0500

    MVCMusicStoreApplication(Lab 6) - Data Validation

--- MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Album.cs
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Album.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCMusicStoreApplication.Models
 {
@@ -10,10 +11,20 @@ namespace MVCMusicStoreApplication.Models
         public virtual int AlbumId { get; set; }
         public virtual int GenreId { get; set; }
         public virtual int ArtistId { get; set; }
+
+        [Display(Name = "Album Title")]
+        [Required(ErrorMessage = "Album Title cannot be blank")]
         public virtual string Title { get; set; }
+
+        [Display(Name = "Album Price")]
+        [Required]
+        [Range(0.01, 100.00, ErrorMessage = "Album price must be between $0.01 and $100.00")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
         public virtual decimal Price { get; set; }
         public virtual Genre Genre { get; set; }
         public virtual Artist Artist { get; set; }
+
+        [Display(Name = "Album Art URL")]
         public virtual string AlbumArtUrl {get;set;}
         public virtual string CountryOfOrigin { get; set; }
         public virtual bool InStock { get; set; }
