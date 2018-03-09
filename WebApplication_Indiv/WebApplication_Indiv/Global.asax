d3e2b44fc99c7470ea5155bfd777a9d790ccabc7
vanithasajja <vanithasajja2010@gmail.com>
2018-03-09 17:43:39 -0500

    Lab 8 - Authentication (Integrated with Google)

---
+++ WebApplication_Indiv/WebApplication_Indiv/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_Indiv.MvcApplication" Language="C#" %>
---
+++ WebApplication_Indiv/WebApplication_Indiv/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication_Indiv.Startup))]
+namespace WebApplication_Indiv
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
