1523a692128548f3285e887bfe1a4d7599486499
vanithasajja <vanithasajja2010@gmail.com>
2018-04-02 13:10:21 -0400

    Lab 11 - Store Controller
    Created a Store Controller and Added Action methods and Views 'Browse', 'Details', 'Index'

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using MVCMusicStoreApplication.Models;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        // GET: Store
+        public ActionResult Index(int id)
+        {
+            var albums = db.Albums.Where(n=>n.GenreId==id).ToList();
+            return View(albums);
+        }
+
+        public ActionResult Browse()
+        {
+            var genres = db.Genres.ToList();
+            return View(genres) ;
+        }
+
+        public ActionResult Details(int id)
+        {
+            var album = db.Albums.Where(n => n.AlbumId == id).FirstOrDefault<Album >() ;
+            return View(album);
+        }
+    }
+}
